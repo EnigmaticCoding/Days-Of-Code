aed731dec4648fdd2dc0f8df08c9b668333b37ef
EnigmaticCoding <lyndseykarow@gmail.com>
2024-01-09 19:12:31 -0500

    completed day 2

--- 2/Day_2.md
+++ 2/Day_2.md
@@ -1,8 +1,7 @@
 # Day 12
 ## Goals
-* Learn more about C# coding language and write a moderately more complex program in c#
-
+* Learn more about C# coding language and write a program in C#
 
 
 ## Tasks completed
-* 
+* created random curse word generator using lists, strings, and Random().
---
+++ 2/random curse word generator/randomCurseWordGenerator/randomCurseWordGenerator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace randomCurseWordGenerator {
+    class Program { 
+    public static void Main(string[] args)
+        {
+            String one = "Aloof,Arrogant,Belligerent,Big-headed,Boastful,Bossy,Callous,Careless,Clingy,Confrontational,Cowardly,Cruel,Cynical,Defensive,Finicky,Foolish,Grumpy,Gullible,Hostile,Idle,Impolite,Inconsiderate,Indecisive,Irresponsible,Moody,Nasty,Overcritical,Overemotional,Patronizing,Pessimistic,Pompous,Self-centered,Silly,Sullen,Thoughtless,Touchy,Untidy,Vain,Weak-willed";
+
+            String[] listOne = one.Split(',');           
+
+            String two = "cabbage,shoe,half-chewed bubblegum,sock,honey,hair,snake,screw,lumber,crack,spider,baby,toothpaste,bottle,fuck,ass,wet bread,marble,ingrown hair,ant,fly,monkey,dog,loser,snail,rusty nail, rag;";
+
+            String[] listTwo = two.Split(',');
+
+            Random rnd = new Random();
+
+            int num1 = rnd.Next(0, listOne.Length);
+            int num2 = rnd.Next(0, listTwo.Length);
+
+            Console.WriteLine("You are a " + listOne[num1] + " " + listTwo[num2] + ".");
+
+
+        } 
+    }
+}
